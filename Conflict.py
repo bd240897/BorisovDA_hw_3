950bfa8757669b195176eaca51cd8c41ab1f88eb
Dmitry <bd240897@yandex.ru>
2020-09-14 17:03:44 +0300

    conflict_3

---
+++ Conflict.py
@@ -0,0 +1,7 @@
+﻿for i in range(1,10) :
+	print(i)
+	print('Тут будет конфликт!!!!!!!!! sadas0dasd asd asd')
+	print('Тут будет конфликт!!!!!!!!!')
+	print('Тут будет конфликт!!!!!!!!!')
+	for j in range(1,10);
+		print(j)
