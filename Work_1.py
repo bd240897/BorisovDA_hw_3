8cb5e0ebc62b7d0a87413cafeaa927aafd2206e9
Dmitry <bd240897@yandex.ru>
2020-09-14 16:56:48 +0300

    1111

---
+++ Work_1.py
@@ -0,0 +1,5 @@
+﻿for i in range(1,10) :
+	print(i)
+	print('Hello git')
+	for j in range(1,10);
+		print(j)
---
+++ raiting.py
@@ -0,0 +1,5 @@
+﻿for i in range(1,10) :
+	print(i)
+	print('My new rating')
+	for j in range(1,10);
+		print(j)
